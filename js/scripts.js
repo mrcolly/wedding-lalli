93c5206e133b470556aed4269026b4b9ff382579
Alessio <alessio.colombo@openmindonline.it>
2019-09-03 22:21:23 +0200

    added photos

--- js/scripts.js
+++ js/scripts.js
@@ -199,7 +199,7 @@ $(document).ready(function () {
             } else {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
-                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+                    "border-bottom": "none",
                     "padding": "50px 0"
                 });
                 $('header .member-actions').css({

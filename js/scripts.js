7af513352e95391ec12ad4e39640c863e8ffbe3b
Alessio Colombo <alessio.colombo@openmindonline.it>
2020-01-20 15:42:53 +0100

    added new logos

--- js/scripts.js
+++ js/scripts.js
@@ -193,7 +193,7 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "25px 0"
                 });
                 $('header .member-actions').css({
                     "top": "26px",
